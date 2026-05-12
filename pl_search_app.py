7afc770f4fd0385e3bda0cc0ba9a10c1fa7b52bd
Pluslub <pluslab.wakatake@gmail.com>
2026-05-12 13:46:53 +0900

    Update pl_search_app.py

--- pl_search_app.py
+++ pl_search_app.py
@@ -234,9 +234,10 @@ def index_channel(sel, token):
             name = user.get('displayName', '不明') if user else '不明'
             created = msg.get('createdDateTime', '')
             msg_id = msg.get('id', '')
-            teams_link =
-f"https://teams.microsoft.com/l/message/{channel_id}/{msg_id}?groupId={team_id}&tenantId={MS_TENANT_ID}"
-            atts = msg.get('attachments', [])
+            tl = "https://teams.microsoft.com/l/message/"
+              tl += channel_id + "/" + msg_id
+              tl += "?groupId=" + team_id + "&tenantId=" + MS_TENANT_ID
+              teams_link = tl
             att_names = [a.get('name', '') for a in atts if a.get('name')]
             full_content = body
             if att_names:
